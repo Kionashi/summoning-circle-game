2284061b5b5d99a2a4d78526ad54d2582ad512c6
Kionashi <cardozo.anibal@gmail.com>
2026-07-29 21:09:41 +0200

    feature - Add summoning circle

--- scenes/drag_layer.gd
+++ scenes/drag_layer.gd
@@ -1,18 +1,12 @@
-extends Area2D
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+func _on_item_drag_ended(_area: Area2D, drop_spot: SnappingSpot) -> void:
+	print('drag ended (on the drag layer)')
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _input(event):
-	if event.is_action_pressed("draggable_click"):
-		print('clicking')
-
-	elif event.is_action_released("draggable_click"):
-		print('release')
--- scenes/shelf.gd
+++ scenes/shelf.gd
@@ -2,21 +2,51 @@ extends Area2D
 
 @export var inventory_trigger: Area2D
 @export var items: Node2D
+@export var dragLayer: CanvasLayer
 
-const INITIAL_POS: Vector2 = Vector2(839.0, -246.0)
-const FINAL_POS: Vector2 = Vector2(818.0, 60.0)
+const INITIAL_POS: Vector2 = Vector2(800.0, -200.0)
+const FINAL_POS: Vector2 = Vector2(800.0, 0.0)
 
 var _is_open: bool = false
 var _tween: Tween
+var _circle_match = {}
 
 func _ready() -> void:
+	self.position = INITIAL_POS
 	inventory_trigger.mouse_entered.connect(_open)
 
 	for item in items.get_children():
-		pass
-		
+		for child in item.find_children("Draggable", "Node", true):
+			if child is Draggable:
+				child.drag_ended.connect(_on_item_drag_ended)
+				child.drag_started.connect(_on_item_drag_started)
 
-func create_unique_tween():
+func _on_item_drag_started(area: Area2D) -> void:
+	area.reparent(dragLayer)
+func _on_item_drag_ended(_area: Area2D, drop_spot: SnappingSpot) -> void:
+	if !drop_spot || !drop_spot.point:
+		return
+	var target_area = drop_spot.point
+	var dragged_item = drop_spot.occupant
+	if target_area and target_area.name.contains('Area') and _is_open and dragged_item:
+		print(target_area.name)
+		print(dragged_item.name)
+		var matches = false
+		if target_area.name.contains(dragged_item.name):
+			matches = true
+		_circle_match[target_area.name] = matches
+		
+		if _circle_match.size() != 6:
+			print(_circle_match)
+			_close()
+			return
+			
+		_close()
+		if (_circle_match.values.all(func(value): return value)):
+			print("success")
+		print("failure")
+
+func _create_unique_tween():
 	if _tween && _tween.is_valid():
 		_tween.kill()
 	_tween = create_tween()
@@ -28,13 +58,13 @@ func toggle_inventory() -> void:
 	_close()
 
 func _open() -> void:
-	create_unique_tween()
+	_create_unique_tween()
 	_tween.tween_property(self, "position", FINAL_POS, 1.0) \
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	_is_open = true
 
 func _close() -> void:
-	create_unique_tween()
+	_create_unique_tween()
 	_tween.tween_property(self, "position", INITIAL_POS, 1.0) \
 		.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
 	_is_open = false
